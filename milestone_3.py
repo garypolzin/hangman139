0ff02cb07c7f335e62b2da4f7442647889029318
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-12-02 18:48:17 +0000

    Create functions to run the checks

--- milestone_3.py
+++ milestone_3.py
@@ -7,20 +7,29 @@ def play_hangman():
 
     word = get_random_word()
 
-    while True:
-        guess = input("Enter a single letter: ")
-
-        if len(guess) == 1 & guess.isalpha():
-            if guess in word:
-                print(f"Good guess! {guess} is in the word.")
-            else:
-                print(f"Sorry, {guess} is not in the word. Try again.")
-        else:
-            print("Invalid letter. Please, enter a single alphabetical character.")
-
 
 def get_random_word():
     return random.choice(word_list)
 
 
-play_hangman()
+def check_guess(guess, word):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+
+def ask_for_input(word):
+    while True:
+        guess = input("Enter a single letter: ")
+
+        if len(guess) == 1 & guess.isalpha():
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+
+    check_guess(guess, word)
+
+
+ask_for_input("apple")
