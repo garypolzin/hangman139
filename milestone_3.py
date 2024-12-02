a4065e23899694b4ad8f46a5dd5efc4c9db17387
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-12-02 18:35:47 +0000

    Check whether guess is in the word

--- milestone_3.py
+++ milestone_3.py
@@ -1,7 +1,26 @@
-while True:
-    guess = input("Enter a single letter: ")
+import random
 
-    if len(guess) == 1 & guess.isalpha():
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+word_list = ["banana", "apple", "mango", "pineapple", "orange"]
+
+
+def play_hangman():
+
+    word = get_random_word()
+
+    while True:
+        guess = input("Enter a single letter: ")
+
+        if len(guess) == 1 & guess.isalpha():
+            if guess in word:
+                print(f"Good guess! {guess} is in the word.")
+            else:
+                print(f"Sorry, {guess} is not in the word. Try again.")
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+
+
+def get_random_word():
+    return random.choice(word_list)
+
+
+play_hangman()
