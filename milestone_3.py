46e589ff8639402d5af95e82e2b076b04f34bc8f
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-12-04 17:57:09 +0000

    lower case guess and refactor

--- milestone_3.py
+++ milestone_3.py
@@ -1,10 +1,11 @@
 import random
 
 word_list = ["banana", "apple", "mango", "pineapple", "orange"]
+word = random.choice(word_list)
 
 
-def check_guess(guess):
-    word = random.choice(word_list)
+def check_guess(guess: str) -> None:
+    guess = guess.lower()
 
     if guess in word:
         print(f"Good guess! {guess} is in the word.")
